929755d006ef443c5b7d3838a767f1e433ad1c3e
이기운 <slehdejfj@gmail.com>
2023-11-09 17:43:26 +0900

    parcel

--- app/page.tsx
+++ app/page.tsx
@@ -222,7 +222,7 @@ export default function Home() {
 
     <>
     {
-      isLoading === true &&
+      isLoading === true ?
         <div className="fixed w-full h-full bg-black/50 top-0 left-0 z-50">
           <div className="absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4">
           <svg width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
@@ -278,6 +278,7 @@ export default function Home() {
             </svg>
           </div>
         </div>
+        : ""
 
     }
 
